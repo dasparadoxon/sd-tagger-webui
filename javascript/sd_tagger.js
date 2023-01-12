5f9cdf882bb85624c65ca07e41c8c9c89ff34614
SesuMoe <>
2023-01-11 23:08:09 -0600

    Formatting

--- javascript/sd_tagger.js
+++ javascript/sd_tagger.js
@@ -1,24 +1,3 @@
-const observeDOM = (function(){
-    const MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
-        eventListenerSupported = window.addEventListener;
-
-    return function(obj, callback){
-        if( MutationObserver ){
-            // define a new observer
-            let obs = new MutationObserver(function(mutations, observer){
-                if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
-                    callback();
-            });
-            // have the observer observe foo for changes in children
-            obs.observe( obj, { childList:true, subtree:true });
-        }
-        else if( eventListenerSupported ){
-            obj.addEventListener('DOMNodeInserted', callback, false);
-            obj.addEventListener('DOMNodeRemoved', callback, false);
-        }
-    };
-})();
-
 let onPageLoad = () => {
     let tg = gradioApp().querySelector("#tag_list");
     let tgli = gradioApp().querySelector("#tag_list_inner");
--- scripts/helpers/tagger.py
+++ scripts/helpers/tagger.py
@@ -1,80 +1,85 @@
 import os
-import sys
 import re
-import numpy
+
 
 class DatasetImage:
-	def __init__(self, path):
-		self.path = path
-		self.tagfile = get_tagfile(path)
-		self.tags = []
-		self.load()
+    def __init__(self, path):
+        self.path = path
+        self.tagfile = get_tagfile(path)
+        self.tags = []
+        self.load()
 
-	def save(self):
-		write_tagfile(self.tagfile, self.tags)
+    def save(self):
+        write_tagfile(self.tagfile, self.tags)
 
-	def load(self):
-		self.tags = read_tagfile(self.tagfile)
+    def load(self):
+        self.tags = read_tagfile(self.tagfile)
 
-	def __str__(self):
-		return f"{self.path}-{self.tags}"
+    def __str__(self):
+        return f"{self.path}-{self.tags}"
+
 
 class Tagger:
-	def __init__(self, path):
-		self.index = 0
-		self.path = path
-		self.dataset = self.loadDataset(path)
-		self.num_files = len(self.dataset)
-	
-	def set(self, index):
-		self.index = index
-		if self.index >= self.num_files:
-			self.index = 0
-		if self.index < 0:
-			self.index = self.num_files - 1
+    def __init__(self, path):
+        self.index = 0
+        self.path = path
+        self.dataset = self.loadDataset(path)
+        self.num_files = len(self.dataset)
 
-	def next(self):
-		self.set(self.index + 1)
+    def set(self, index):
+        self.index = index
+        if self.index >= self.num_files:
+            self.index = 0
+        if self.index < 0:
+            self.index = self.num_files - 1
 
-	def previous(self):
-		self.set(self.index - 1)
+    def next(self):
+        self.set(self.index + 1)
 
-	def current(self):
-		return self.dataset[self.index]
+    def previous(self):
+        self.set(self.index - 1)
 
-	def loadDataset(self, path):
-		files = recursive_dir(path, '.png')
-		files = sort(files)
-		dataset = []
-		for f in files:
-			dataset.append(DatasetImage(f))
-		return dataset
+    def current(self):
+        return self.dataset[self.index]
 
-## Helper Functions ##
+    def loadDataset(self, path):
+        files = recursive_dir(path, '.png')
+        files = sort(files)
+        dataset = []
+        for f in files:
+            dataset.append(DatasetImage(f))
+        return dataset
+
+
+# Helper Functions #
 def get_tagfile(path):
-	return os.path.splitext(path)[0] + '.txt'
+    return os.path.splitext(path)[0] + '.txt'
+
 
 def read_tagfile(tagfile):
-	if os.path.isfile(tagfile):
-		with open(tagfile, 'r') as f:
-			return [t for t in f.read().strip().split(', ') if len(t) > 0]
+    if os.path.isfile(tagfile):
+        with open(tagfile, 'r') as f:
+            return [t for t in f.read().strip().split(', ') if len(t) > 0]
+
 
 def write_tagfile(tagfile, tags):
-	with open(tagfile, 'w') as f:
-		f.write(', '.join(tags))
+    with open(tagfile, 'w') as f:
+        f.write(', '.join(tags))
 
-def sort(l): 
-	convert = lambda text: int(text) if text.isdigit() else text 
-	alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
-	return sorted(l, key = alphanum_key)
+
+def sort(l):
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key=alphanum_key)
+
 
 def recursive_dir(path, ext):
-	r = []
-	for f in os.listdir(path):
-		jf = os.path.join(path, f)
-		if os.path.isdir(jf):
-			r += recursive_dir(jf, ext)
-		elif os.path.isfile(jf):
-			if jf.lower().endswith(ext):
-				r.append(jf)
-	return r
+    r = []
+    for f in os.listdir(path):
+        jf = os.path.join(path, f)
+        if os.path.isdir(jf):
+            r += recursive_dir(jf, ext)
+        elif os.path.isfile(jf):
+            if jf.lower().endswith(ext):
+                r.append(jf)
+    return r
